5229c591babc94bb860123ad3ddf972a011ef93c
Paul Tobin <tobster111@gmail.com>
2022-02-25 16:44:39 +0000

    _result

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -7,7 +7,6 @@ class StocksController < ApplicationController
         respond_to do |format|
           format.js { render partial: 'users/result' }
         end
-        render 'users/my_portfolio'
       else
         flash[:alert] = "Please enter a valid symbol to search"
         redirect_to my_portfolio_path
