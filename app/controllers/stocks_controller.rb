fe5a2555ab977333fd1fe73a00afc4e39d878934
Paul Tobin <tobster111@gmail.com>
2022-02-26 10:36:19 +0000

    Using format.js to response to the ajax request

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,12 +8,16 @@ class StocksController < ApplicationController
           format.js { render partial: 'users/result' }
         end
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+          flash[:alert] = "Please enter a valid symbol to search"
+        end
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        format.js { render partial: 'users/result' }
+        flash[:alert] = "Please enter a symbol to search"
+      end
     end
   end
   
