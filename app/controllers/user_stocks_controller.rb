87274fcf825030ea5179d7a28c4a8ddd8f69b22a
Paul Tobin <tobster111@gmail.com>
2022-02-27 12:44:58 +0000

    create finished

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -1,9 +1,12 @@
 class UserStocksController < ApplicationController
   def create
-    
-    stock = Stock.new_lookup(params[:ticker])
+    stock = Stock.check_db(params[:ticker])
+    if stock.blank?
+      stock = Stock.new_lookup(params[:ticker])
+      stock.save
     @user_stock = UserStock.create(user: current_user, stock: stock)
-    
+    flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
+    redirect_to my_portfolio_path
   end
   
 end
