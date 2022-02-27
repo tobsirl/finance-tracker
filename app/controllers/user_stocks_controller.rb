e11cd3504e3fa343f8a7614fed9bc47533a4f3f3
Paul Tobin <tobster111@gmail.com>
2022-02-27 12:46:08 +0000

    fixed bug

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -4,6 +4,7 @@ class UserStocksController < ApplicationController
     if stock.blank?
       stock = Stock.new_lookup(params[:ticker])
       stock.save
+    end
     @user_stock = UserStock.create(user: current_user, stock: stock)
     flash[:notice] = "Stock #{stock.name} was successfully added to your portfolio"
     redirect_to my_portfolio_path
