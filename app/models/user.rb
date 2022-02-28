c280a9d99d2277f2ea6af8d315e71c582750d370
Paul Tobin <tobster111@gmail.com>
2022-02-28 19:31:03 +0000

    stock_already_tracked>

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+  
 
   def under_stock_limit?
     stocks.count < 10
