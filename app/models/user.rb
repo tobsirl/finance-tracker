85e78db5f073f515fd6a3f914fbb8e912a4d5239
Paul Tobin <tobster111@gmail.com>
2022-02-28 19:32:36 +0000

    using both methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ApplicationRecord
   end
 
   def can_track_stock?(ticker_symbol)
-    if user.stocks.count < 10
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
   
 end
