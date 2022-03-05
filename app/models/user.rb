cf2b7a5d0c93e8d457dc42523eba8bd575582428
Paul Tobin <tobster111@gmail.com>
2022-03-05 10:30:50 +0000

    search complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,7 +31,9 @@ class User < ApplicationRecord
 
   def self.search(param)
     param.strip!
-
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back  
   end
 
   def self.first_name_matches(param)
